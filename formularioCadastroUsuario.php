c41ac2407c883c678d6384a758bb06a38828db4e
anabitencourt <48722988+anabitencourt@users.noreply.github.com>
2020-05-11 16:37:51 -0300

    Add files via upload

---
+++ formularioCadastroUsuario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head> 
+        <meta charset="uft-8">
+        <title>CADASTRO USUÁRIO</title>
+</head>
+<body>
+     <center>
+        <h1> CADASTRO DE USUÁRIO </h1>  
+        <form class="" action="controlerUsuario.php" method="POST">
+        <br><label for=""> Nome: </label><br>   
+            <br><input type="text" name="nome" value=""><br>
+
+        <br><label for=""> E-mail: </label><br>
+            <br><input type="email" name="email" value=""><br>
+
+        <br><label for=""> Login: </label><br>
+            <br><input type="text" name="login" value=""><br>
+
+        <br><label for=""> Senha: </label><br>
+            <br><input type="password" name="senha" value=""><br>
+ 
+        <br><input type="submit" name="" value="CADASTRAR"><br>        
+        </center>
+</body>
+</html>
