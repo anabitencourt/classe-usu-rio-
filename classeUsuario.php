9717af0cb6f0e68a9b4716fb1087025adfb96c29
anabitencourt <48722988+anabitencourt@users.noreply.github.com>
2020-05-11 15:10:46 -0300

    Add files via upload

---
+++ classeUsuario.php
@@ -0,0 +1,42 @@
+class Usuario{
+    private $id, $nome, $email, $login, $senha, $dataCadastro, $ativo;
+
+    public function getId(){
+        return $this ->id;
+    }
+
+    public function getNome(){
+        return $this ->nome;
+    }
+
+    public function getEmail(){
+        return $this ->email;
+    }
+
+    public function getLogin(){
+        return $this ->login;
+    }
+
+    public function getDataCadastro(){
+        return $this ->dataCadastro;
+    }
+
+    public function getAtivo(){
+        return $this ->ativo;
+    }
+       
+    public function renovarSenha(){
+        $this ->senha = "123456";
+    }
+
+    public function addusario($nome, $email, $login, $senha){
+        $this->nome = $nome;
+        $this->email = $email,
+        $this->login = $login;
+        $this->senha = $senha;
+        $this->dataCadastro = date("Y-m-d");
+        $this->ativo = 0;
+    } 
+
+
+}
